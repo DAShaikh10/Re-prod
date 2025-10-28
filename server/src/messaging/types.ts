4cae0fea4c261d2e61e068430fded44f9879466a
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2025-10-28 12:17:18 +0900

    Remove Node.js server (replaced by Rust/Axum)

--- server/src/messaging/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Server, Socket } from 'socket.io';
-import type { ClientToServerEvents, ServerToClientEvents } from '@shared/types';
-
-export type AppServer = Server<ClientToServerEvents, ServerToClientEvents>;
-export type AppSocket = Socket<ClientToServerEvents, ServerToClientEvents>;
