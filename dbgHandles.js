68ce0987526948011ce80cc04597a8a17e717068
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2025-11-15 15:32:10 +0900

    chore: remove unnecessary debug and test files

--- dbgHandles.js
+++
@@ -1,7 +0,0 @@
-const logHandles = () => {
-  const handles = process._getActiveHandles().map((handle) => handle.constructor?.name ?? 'unknown');
-  console.log('DBG handles', handles.join(', '));
-};
-setTimeout(logHandles, 5000);
-setTimeout(logHandles, 10000);
-setTimeout(logHandles, 15000);
